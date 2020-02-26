6227ee8441a56c195a87fed14c1e037f79f69d41
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2020-02-26 14:54:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n *= -1;
+        n = String(n);
+        n = n.split('');
+        n = n.reverse();
+        n = n.join('');
+        n = Number(n) ;
+    } else {
+        n = String(n);
+        n = n.split('');
+        n = n.reverse();
+        n = n.join('');
+        n = Number(n);
+    }
+    return n;
 }
